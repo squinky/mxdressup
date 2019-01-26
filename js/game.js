e1528fcab00aebbb25148849f1ad9a1f7e82afae
Squinky <hey@squinky.me>
2019-01-25 20:33:18 -0500

    Draggy droppy

--- js/game.js
+++ js/game.js
@@ -1,8 +1,31 @@
 var bg;
+var clothing = [];
 
 function initGame()
 {
 	bg = new createjs.Bitmap(queue.getResult("mockup"));
+
+	initClothingItem("mockup-hat", 200, 250);
+	initClothingItem("mockup-shirt", 300, 500);
+}
+
+function initClothingItem(name, xpos, ypos)
+{
+	var c = {};
+	c = new createjs.Bitmap(queue.getResult(name));
+	c.regX = c.getBounds().width/2;
+	c.regY = c.getBounds().height/2;
+	c.x = xpos;
+	c.y = ypos;
+
+	c.on("pressmove", function(evt)
+	{
+		var newPos = stage.globalToLocal(evt.stageX, evt.stageY);
+    	evt.target.x = newPos.x;
+    	evt.target.y = newPos.y;
+	});
+
+	clothing.push(c);
 }
 
 function startGame()
@@ -10,4 +33,10 @@ function startGame()
 	currentScreen = SCREEN_GAME;
 
 	stage.addChild(bg);
+
+	var n;
+	for (n of clothing)
+	{
+		stage.addChild(n);
+	}
 }
