0dacf8274ef808f524eef0c1995ec8b14d0f2ba3
Squinky <hey@squinky.me>
2019-01-26 23:00:53 -0500

    Separated faces and added refresh button

--- js/game.js
+++ js/game.js
@@ -1,9 +1,10 @@
-var bg, body, skinToneChanger, currentSkinTone;
+var bg, body, skinToneChanger, currentSkinTone, refresh;
 var bodies = [];
 var clothing = [];
+var faces = [];
 
 var PILE_MIN_X = 75;
-var PILE_MIN_Y = 250;
+var PILE_MIN_Y = 450;
 var PILE_MAX_X = 1150;
 var PILE_MAX_Y = 950;
 
@@ -14,7 +15,7 @@ function initGame()
 	bg = new createjs.Bitmap(queue.getResult("bg"));
 
 	body = new createjs.Container();
-	body.x = 1200;
+	body.x = 1150;
 	for (var i = 1; i <= 9; i++)
 	{
 		var bodybmp = new createjs.Bitmap(queue.getResult("col0"+i));
@@ -25,7 +26,7 @@ function initGame()
 	body.setChildIndex(bodies[currentSkinTone], body.numChildren-1);
 
 	skinToneChanger = new createjs.Bitmap(queue.getResult("skintonechanger"));
-	skinToneChanger.x = 1300;
+	skinToneChanger.x = 1250;
 	skinToneChanger.y = 100;
 	skinToneChanger.cursor = "pointer";
 	skinToneChanger.on("click", function(evt)
@@ -33,6 +34,15 @@ function initGame()
 		currentSkinTone++;
 		if (currentSkinTone == bodies.length) currentSkinTone = 0;
 		body.setChildIndex(bodies[currentSkinTone], body.numChildren-1);
+	});
+
+	refresh = new createjs.Bitmap(queue.getResult("refresh"));
+	refresh.x = 1700;
+	refresh.y = 750;
+	refresh.cursor = "pointer";
+	refresh.on("click", function(evt)
+	{
+		restartGame();
 	});
 
 	initClothingItems("face");
@@ -68,6 +78,7 @@ function startGame()
 	stage.addChild(bg);
 	stage.addChild(body);
 	stage.addChild(skinToneChanger);
+	stage.addChild(refresh);
 
 	changeInspirationText();
 	stage.addChild(prompt);
@@ -78,6 +89,8 @@ function startGame()
 	{
 		placeClothingItem(n);		
 	}
+	
+	placeFaces();
 }
 
 function restartGame()
@@ -111,7 +124,8 @@ function initClothingItems(type)
 	    	evt.target.y = newPos.y - c.posOnObject.y;
 		});
 
-		clothing.push(c);
+		if (type == "face") faces.push(c);
+		else clothing.push(c);
 		i++;
 	}
 }
@@ -132,6 +146,16 @@ function placeClothingItem(item)
 	var zindex = Math.floor(Math.random() * stage.numChildren) + 1;
 
 	stage.addChildAt(item, zindex);
+}
+
+function placeFaces()
+{
+	for (var i = 0; i < faces.length; i++)
+	{
+		faces[i].x = 75 + (1075/faces.length)*i;
+		faces[i].y = 250 + 50*(i%2);
+		stage.addChild(faces[i]);
+	}
 }
 
 function changeInspirationText()
